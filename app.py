02a72439382019461657c6971b0439aff755e8fc
JonyDuarte <joaoc84duarte@gmail.com>
2024-09-25 21:13:04 -0300

    versão1.0

---
+++ app.py
@@ -0,0 +1,14 @@
+# importar bibliotecas
+import os
+import time
+
+# entrada de dados
+n = int(input("Informe um número inteiro positivo: "))
+
+while n >= 0:
+    os.system("cls")
+    print(n)
+    time.sleep(1)
+    n -= 1
+os.system("cls")
+print("BOOOOM!!!!")
